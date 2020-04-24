3cef49c46733ec74187d4569c6afc4b38b0a2c41
Anatolii <>
2020-04-24 22:26:03 +0300

    change main class name

--- src/TransManagerBundle.php
+++ src/TransManagerBundle.php
@@ -6,8 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Terra\TransManagerBundle\DependencyInjection\CompilerPass\TransManagerCompilerPass;
 
-class TransManagerBundle extends Bundle {
-
+class TerraTransManagerBundle extends Bundle {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
